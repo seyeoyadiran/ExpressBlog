adc9db69103dd78072c4b9ff7242f3e0edacd20c
seyeoyadiran <seyeoyadiran@gmail.com>
2024-11-21 00:19:50 -0500

    Making frequent Commits

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const Article  = require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express();
 const url = "mongodb://0.0.0.0:27017";
@@ -14,17 +15,10 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false}))
 
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test description'
-    }, 
-    { 
-        title: 'Test Article 2',
-        createdAt: new Date(),
-        description: 'Test description 2' 
-    }]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort( {
+        createdAt: 'desc'
+    });
     res.render('articles/index', { articles: articles })
 })
 
